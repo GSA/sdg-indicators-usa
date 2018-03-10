58f8834b002c2a04438ad7cbd568c98d78fc64cf
Brock Fanning <brockfanning@gmail.com>
2018-03-10 10:26:59 -0500

    Use Github API to get usernames

--- scripts/validate/github-user-check.py
+++ scripts/validate/github-user-check.py
@@ -1,46 +1,78 @@
 # -*- coding: utf-8 -*-
 """
 This is a validation script to make sure that pending pull requests have been
-submitted by a valid user.
+submitted by a valid user. This script is mostly informed by the Yaml file in
+the same folder: github-user-list.yml.
 """
 
 import os.path
+import sys
 import yaml
 import subprocess
+import requests
 
 print('Checking user permissions...')
 
 # Get the github-users.yml file data.
-scripts_path = os.path.join('scripts', 'validate', 'github-users.yml')
-with open(scripts_path, 'r') as stream:
+file_path = os.path.join('scripts', 'validate', 'github-user-list.yml')
+with open(file_path, 'r') as stream:
     try:
         users = yaml.load(stream)
     except yaml.YAMLError as e:
         print(e)
+        sys.exit(1)
 
-# Get the author of the commits.
-cmd = ['git', 'log', '--format="%ae"', 'HEAD^!']
-user = subprocess.check_output(cmd).decode('utf-8').strip().strip('"')
-
-# Exit now if the author is not in our list.
-if user not in users:
-  raise RuntimeError('User "' + user + '" is not in approved list of users.')
+# Load the Jekyll _config.yml to get the Github repository information.
+file_path = '_config.yml'
+with open(file_path, 'r') as stream:
+    try:
+        site = yaml.load(stream)
+    except yaml.YAMLError as e:
+        print(e)
+        sys.exit(1)
 
 # Get the files changed in the commits.
-cmd = ['git', 'diff-tree', '--no-commit-id', '--name-only', '-r', 'HEAD']
+cmd = ['git', 'diff-tree', '--name-only', '-r', 'HEAD']
 proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-for line in proc.stdout.readlines():
-  change = line.decode('utf-8').strip()
-  # Validate the list of indicators if specified.
-  if 'indicators' in users[user]:
-    for indicator in users[user]['indicators']:
-      if indicator not in change:
-        raise RuntimeError('Changed file "' + change + '" is outside list of indicators for user "' + user + '".')
-  # Validate the list of folders if specified.
-  if 'folders' in users[user]:
-    for folder in users[user]['folders']:
-      if folder not in change:
-        raise RuntimeError('Changed file "' + change + '" is outside list of folders for user "' + user + '".')
-  print('-- change permitted: ' + change)
 
+# Loop through the lines of output that the above Git command produced.
+for index, line in enumerate(proc.stdout.readlines()):
+  change = line.decode('utf-8').strip()
+  if index == 0:
+    # The first line is the commit ID, which we use to get the Github user.
+    commit_id = change
+    github_api_request = "https://api.github.com/repos/%s/%s/commits/%s" \
+      % (site['org_name'], site['repo_name'], commit_id)
+    try:
+      response = requests.get(github_api_request).json()
+    except requests.exceptions.RequestException as e:
+      print(e)
+      sys.exit(1)
+
+    # Exit now if the author is not in our list.
+    user = response['author']['login']
+    if user not in users:
+      raise RuntimeError("User %s is not in approved list of users." % user)
+
+    # Also exit now if the author is an administrator.
+    if 'administrator' == users[user]:
+      print("-- User %s is an administrator: skipping Github user check." % user)
+      sys.exit(0)
+
+  else:
+    # Validate the list of indicators if specified.
+    if 'indicators' in users[user]:
+      for indicator in users[user]['indicators']:
+        if indicator not in change:
+          raise RuntimeError("Changed file '%s' is outside list of indicators for user '%s'." % (change, user))
+    # Validate the list of folders if specified.
+    if 'folders' in users[user]:
+      for folder in users[user]['folders']:
+        if folder not in change:
+          raise RuntimeError("Changed file '%s' is outside list of folders for user '%s'." % (change, user))
+
+    # If still here, give feedback.
+    print("-- change permitted: %s" % change)
+
+# No exceptions, so we report success.
 print('Success')
